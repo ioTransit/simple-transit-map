21b7b73cf05da3e7f117c45b64d7eae093d5a544
AvidDabbler <walter.k.jenkins@gmail.com>
2023-11-14 10:07:52 -0600

    idk

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig(() => {
   return {
-    define: {
-      // "process.env.VITE_APP_MAPBOX_KEY": env.VITE_APP_MAPBOX_KEY,
-      // "process.env.GTFS_URLS": env.GTFS_URLS,
-    },
+    define: {},
     plugins: [react()],
   };
 });
