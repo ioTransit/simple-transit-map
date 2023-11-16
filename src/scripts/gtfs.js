de7c6ee54b2c322e4a7cc0a8c49800c95747c29b
AvidDabbler <walter.k.jenkins@gmail.com>
2023-11-16 14:24:52 -0600

    add in the new gtfs-parser

--- src/scripts/gtfs.js
+++ src/scripts/gtfs.js
@@ -1,46 +1,9 @@
 import * as glob from "glob";
 import fs from "fs";
-import AdmZip from "adm-zip";
-import axios from "axios";
-import gtfs2geojson from "gtfs2geojson";
 import { GTFS_URLS } from "../../config/env";
 import path from "path";
+import { Gtfs } from "gtfs-parser/index";
 
-const createGeojsonFromGtfs = async (url) => {
-  const body = await axios.get(url, {
-    responseType: "arraybuffer",
-  });
-  const zip = new AdmZip(body.data); // eslint-disable-line
-  const zipEntries = zip.getEntries(); // eslint-disable-line
-  if (!zipEntries || !zipEntries.length) return;
-
-  const getFile = (fileName) =>
-    zipEntries.find((zipEntry) => {
-      return zipEntry.entryName === fileName;
-    });
-
-  const stopsFile = getFile("stops.txt");
-  const tripsFile = getFile("trips.txt");
-  const routesFile = getFile("routes.txt");
-  const shapesFile = getFile("shapes.txt");
-
-  const stopsGeojson = stopsFile
-    ? gtfs2geojson.stops(zip.readAsText(stopsFile.entryName))
-    : null;
-  const routesGeojson =
-    tripsFile && routesFile && shapesFile
-      ? gtfs2geojson.routes(
-          zip.readAsText(shapesFile.entryName),
-          zip.readAsText(routesFile.entryName),
-          zip.readAsText(tripsFile.entryName),
-        )
-      : null;
-  const tripsGeojson = tripsFile
-    ? gtfs2geojson.lines(zip.readAsText(tripsFile.entryName))
-    : null;
-
-  return { stopsGeojson, routesGeojson, tripsGeojson };
-};
 const createEmptyFile = async (filePath) => {
   try {
     fs.writeFile(filePath, "", (error) => {
@@ -58,8 +21,12 @@ const createEmptyFile = async (filePath) => {
 const loopThroughAgencies = async () => {
   for (let i = 0; i < GTFS_URLS.length; i++) {
     const agencyUrl = GTFS_URLS[i];
-    const { stopsGeojson, routesGeojson, tripsGeojson } =
-      await createGeojsonFromGtfs(agencyUrl);
+    const gtfs = new Gtfs(agencyUrl);
+    await gtfs.init();
+
+    const tripsGeojson = await gtfs.tripsToGeojson();
+    const routesGeojson = await gtfs.routesToGeojson();
+    const stopsGeojson = await gtfs.stopsToGeojson();
 
     await createEmptyFile(`./public/${i}-trips.json`);
     await createEmptyFile(`./public/${i}-routes.json`);
@@ -90,4 +57,5 @@ export function generateCatalogue() {
 
   console.log("Catalogue generated successfully.");
 }
+
 loopThroughAgencies();
