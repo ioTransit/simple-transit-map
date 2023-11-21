2f123f1ed7914405d68fa0cf24f670da6bf2df48
Walter Jenkins <walter.k.jenkins@gmail.com>
2023-11-21 14:23:13 -0600

    flex areas on panel

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,32 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import { useCallback, useEffect, useState } from "react";
 import { FlexAreas, Routes, Stops } from "./SystemLayers";
 import mapboxgl, { LngLatBoundsLike } from "mapbox-gl";
-import { FeatureCollection, MultiLineString, Position } from "geojson";
+import {
+  FeatureCollection,
+  MultiLineString,
+  MultiPolygon,
+  Position,
+} from "geojson";
 import { sortBy } from "lodash";
 import clsx from "clsx";
 //@ts-expect-error issue with import
 import * as turf from "@turf/turf";
 //@ts-expect-error issue with import
 import bbox from "@turf/bbox"; // es-lint-disable-line
+import { z } from "zod";
+
+const zFlexArea = z.object({
+  name: z.string().min(1),
+  "fill-color": z.string().optional(),
+});
 
 function App() {
   const [filter, setFilter] = useState<string | null>(null);
   const [bounds, setBounds] = useState<LngLatBoundsLike | null>(null);
+  const [flexAreas, setFlexAreas] = useState<FeatureCollection<
+    MultiPolygon,
+    z.infer<typeof zFlexArea>
+  > | null>(null);
   const [routes, setRoutes] = useState<FeatureCollection<
     MultiLineString,
     Route
@@ -25,6 +40,20 @@ function App() {
     const resp = await fetch("bounds.json");
     const _bounds = (await resp.json()) as LngLatBoundsLike;
     setBounds(_bounds);
+  };
+
+  const setupFlexAreas = async () => {
+    try {
+      const resp = await fetch("flex-areas.geojson");
+      const areas = (await resp.json()) as FeatureCollection<
+        MultiPolygon,
+        z.infer<typeof zFlexArea>
+      >;
+      setFlexAreas(areas);
+    } catch (e) {
+      console.warn("no flex areas found");
+      return;
+    }
   };
 
   const setupLayers = async () => {
@@ -42,6 +71,7 @@ function App() {
   useEffect(() => {
     setupLayers();
     setupBounds();
+    setupFlexAreas();
   }, []);
 
   return (
@@ -59,25 +89,28 @@ function App() {
           <Routes filter={filter}></Routes>
           <Stops filter={filter}></Stops>
           <Tools></Tools>
-          <RoutesPanel
+          <LayersPanel
             filter={filter}
             routes={routes}
+            flexAreas={flexAreas}
             setFilter={setFilter}
             bounds={bounds}
-          ></RoutesPanel>
+          ></LayersPanel>
         </Map>
       )}
     </>
   );
 }
 
-const RoutesPanel = ({
+const LayersPanel = ({
   routes,
+  flexAreas,
   filter,
   setFilter,
   bounds,
 }: {
   routes: FeatureCollection<MultiLineString, Route> | null;
+  flexAreas: FeatureCollection<MultiPolygon, z.infer<typeof zFlexArea>> | null;
   filter: string | null;
   setFilter: (e: string | null) => void;
   bounds: LngLatBoundsLike;
@@ -154,6 +187,44 @@ const RoutesPanel = ({
               );
             })}
       </div>
+      {flexAreas && (
+        <>
+          {" "}
+          <h2 className="layer-group-panel">Flex Areas</h2>
+          {flexAreas && (
+            <div className="layer-group">
+              {flexAreas.features
+                .sort((a, b) => {
+                  if (!a.properties.name || !a.properties.name) return 0;
+                  else if (a.properties.name > b.properties.name) return 1;
+                  else return 0;
+                })
+                .map((area) => {
+                  return (
+                    <button
+                      key={area.properties.name}
+                      onClick={() => clickButton(area.properties.name || null)}
+                      className={clsx(
+                        "layer-button",
+                        filter === area.properties.name && "active",
+                      )}
+                    >
+                      {
+                        <div
+                          className="route-color-symbol"
+                          style={{
+                            backgroundColor: `#${area.properties["fill-color"]}`,
+                          }}
+                        ></div>
+                      }
+                      <span>{area.properties.name}</span>
+                    </button>
+                  );
+                })}
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
